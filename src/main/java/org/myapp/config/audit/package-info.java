b58958837e75b77b18557adc802d4631dd98abb0
dkcy <danielckim05@gmail.com>
2018-03-24 16:36:49 -0400

    Initial application generated by JHipster-4.14.1

---
+++ src/main/java/org/myapp/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package org.myapp.config.audit;
---
+++ src/main/java/org/myapp/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package org.myapp.repository;
+
+import org.myapp.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
